6151c69c1779ca2b9010bf59454e5baefb08daac
Nick Lin <zidonglin@outlook.com>
2018-08-09 11:36:33 +0800

    Add: custom the webview

--- App.js
+++ App.js
@@ -37,12 +37,25 @@ const styles = StyleSheet.create({
     fontSize:50,
     color:'#ffffff',
   },
+  wrapper: {
+  },
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
 });
 
 export default class App extends Component/*<Props>*/ {
   render() {
     return (
       Immersive.on(),
+      //WebViewbase
       <SwiperView />
     );
   }
@@ -69,6 +82,18 @@ class SwiperView extends Component{
         <View style={styles.slide}>
           <CusormWebView showURL={'https://gz.kangyun3d.com/models/models/2072ffd0baee400794af828092aac49a/index.html'}/>
         </View>
+        {/* <View style={styles.slide}>
+          <CusormWebView showURL={'https://gz.kangyun3d.com/models/models/14/index.html'}/>
+        </View>
+        <View style={styles.slide}>
+          <CusormWebView showURL={'https://gz.kangyun3d.com/models/models/24/index.html'}/>
+        </View> */}
+        <View style={styles.slide}>
+          <CusormWebView showURL={'https://gz.kangyun3d.com/models/models/dinosaur1/index.html'}/>
+        </View>
+        {/* <View style={styles.slide}>
+          <CusormWebView showURL={'https://gz.kangyun3d.com/models/models/2/index.html'}/>
+        </View> */}
       </Swiper>
     );
   }
@@ -87,7 +112,60 @@ class CusormWebView extends Component{
   }
   
   render(){
-    
+    var tShowURL = this.props.showURL;
+    let WhenLoadWebView = ()=>{
+      //var e = document.createEvent('MouseEvents'); e.initEvent('click', true, true);document.querySelector('.landing').dispatchEvent(e);
+      window.prefetchedData["embedOptions"] = {"preload": true,"autostart": true}
+    }
+    let BeforeLoadWebView = ""//'window.prefetchedData["embedOptions"] = {"preload": true,"autostart": true}';
+    //+"var e = document.createEvent('MouseEvents'); e.initEvent('click', true, true);document.querySelector('.landing').dispatchEvent(e);"
+    //+"var a = setTimeout(function() {var e = document.createEvent('MouseEvents'); e.initEvent('click', true, true);document.querySelector('.icon-ion-close-round-icon').dispatchEvent(e);},100);";
+    return(
+      <View style={{width:'100%',height:'100%'}}>
+        <View style={{width:'100%',height:'100%'}}>
+          <WebView 
+          source={{uri: tShowURL}}
+          mediaPlaybackRequiresUserAction={false}
+          //ref={webview => {this.myWebview = webview;}} 
+          //onLoadStart={()=>{
+          //   this.myWebview.injectJavaScript(BeforeLoadWebView);
+          //}}
+          injectedJavaScript={BeforeLoadWebView}
+          style={{width:'100%',height:'100%'}}/>
+        </View>
+        {/* <View style={{bottom:'40%',flexDirection:'row', direction:'ltr',justifyContent:'space-between'}}>
+          <TouchableHighlight 
+          onPress={this.onPressLearnMore}
+          style={styles.radiasTouchButton}>
+            <View>
+              <Text style={styles.touchButtonText}>←</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight 
+          onPress={this.onPressLearnMore}
+          style={styles.radiasTouchButton}>
+            <View>
+              <Text style={styles.touchButtonText}>→</Text>
+            </View>
+          </TouchableHighlight>
+        </View> */}
+      </View>
     );
   }
 }
+
+// class CustomWebview extends WebView{
+//   state = {curURLIndex:0}
+//   props = {UrlStrArray:[
+//       'http://gz.kangyun3d.com/projects/a9ba3279488e4cc28cbd998d56442b6d/',
+//       'https://gz.kangyun3d.com/models/models/75/index.html',
+//       'https://gz.kangyun3d.com/models/models/93/index.html',
+//       'https://gz.kangyun3d.com/models/models/133/index.html',
+//       'https://gz.kangyun3d.com/models/models/57/index.html',
+//       'https://gz.kangyun3d.com/models/models/dinosaur1/index.html',
+//     ],
+//     curURLIndex:0}
+//     <WebView source={{uri: 'http://gz.kangyun3d.com/projects/a9ba3279488e4cc28cbd998d56442b6d/'}}/>
+// }
+
+//AppRegistry.registerComponent('KangYunExhibit', () => Swiper);
